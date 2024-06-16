df051b0dba82d298e221be8542a68ace56b8e536
Amit Roy <amitroy497@yahoo.com>
2024-06-16 21:43:21 +0530

    feat(taking-photos-in-ios-managing-permissions): taking photos in ios and managing permission

--- src/components/Places/ImagePicker.js
+++ src/components/Places/ImagePicker.js
@@ -1,8 +1,34 @@
-import { launchCameraAsync } from 'expo-image-picker';
-import { Button, View } from 'react-native';
+import {
+	launchCameraAsync,
+	PermissionStatus,
+	useCameraPermissions,
+} from 'expo-image-picker';
+import { Alert, Button, View } from 'react-native';
 
 export const ImagePicker = () => {
+	const [cameraPermissioninformation, requestPermission] =
+		useCameraPermissions();
+
+	const verifyPermissions = async () => {
+		if (cameraPermissioninformation.status === PermissionStatus.UNDETERMINED) {
+			const permissionResponse = await requestPermission();
+			return permissionResponse.granted;
+		}
+		if (cameraPermissioninformation.status === PermissionStatus.DENIED) {
+			Alert.alert(
+				'Insufficient permissions',
+				'You need to grant camera permissions to use this app'
+			);
+			return false;
+		}
+		return true;
+	};
+
 	const takeImageHandler = async () => {
+		const hasPermission = await verifyPermissions();
+		if (!hasPermission) {
+			return;
+		}
 		const image = await launchCameraAsync({
 			allowsEditing: true,
 			aspect: [16, 9],

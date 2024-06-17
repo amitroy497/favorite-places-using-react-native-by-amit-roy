c79c68cc935ed6867d7c3c39688add58f5e73034
Amit Roy <amitroy497@yahoo.com>
2024-06-17 10:15:32 +0530

    feat(image-preview-custom-button): showing an image preview and adding a custom button

---
+++ src/components/UI/OutlinedButton.js
@@ -0,0 +1,42 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { GlobalStyles } from '../../constants';
+
+export const OutlinedButton = ({ children, icon, onPress }) => {
+	return (
+		<Pressable
+			style={(pressed) => [styles.button, pressed && styles.pressed]}
+			onPress={onPress}
+		>
+			<Ionicons
+				style={styles.icon}
+				name={icon}
+				size={18}
+				color={GlobalStyles.colors.primary500}
+			/>
+			<Text style={styles.text}>{children}</Text>
+		</Pressable>
+	);
+};
+
+const styles = StyleSheet.create({
+	button: {
+		paddingHorizontal: 12,
+		paddingVertical: 6,
+		margin: 4,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderWidth: 1,
+		borderColor: GlobalStyles.colors.primary500,
+	},
+	pressed: {
+		opacity: 0.7,
+	},
+	icon: {
+		marginRight: 6,
+	},
+	text: {
+		color: GlobalStyles.colors.primary500,
+	},
+});
